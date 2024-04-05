0bfe52f18646876b7b8b729e586fa66da5eb4fda
Sam Puddister <sampuddister@gmail.com>
2024-04-05 15:54:55 -0400

    Implemented category list.

---
+++ controllers/categoryController.js
@@ -0,0 +1,104 @@
+const Category = require("../models/category");
+const Item = require("../models/item");
+const asyncHandler = require("express-async-handler");
+const { body, validationResult } = require("express-validator");
+
+// Display list of all Categories.
+exports.category_list = asyncHandler(async (req, res, next) => {
+  const allCategories = await Category.find().sort({ name: 1 }).exec();
+
+  res.render("category_list", {
+    title: "Category List",
+    category_list: allCategories,
+  });
+});
+
+// Display detail page for a specific Category.
+exports.category_detail = asyncHandler(async (req, res, next) => {
+  // Get details of category and all associated items (in parallel)
+  const [category, itemsInCategory] = await Promise.all([
+    Category.findById(req.params.id).exec(),
+    Item.find({ category: req.params.id }, "title summary")
+      .sort({ title: 1 })
+      .exec(),
+  ]);
+  if (category === null) {
+    // No results.
+    const err = new Error("Category not found");
+    err.status = 404;
+    return next(err);
+  }
+
+  res.render("category_detail", {
+    title: "Category Detail",
+    category: category,
+    category_items: itemsInCategory,
+  });
+});
+
+// Display Category create form on GET.
+exports.category_create_get = (req, res, next) => {
+  res.render("category_form", { title: "Create Category" });
+};
+
+// Handle Category create on POST.
+exports.category_create_post = [
+  // Validate and sanitize the name field.
+  body("name", "Category name must contain at least 3 characters")
+    .trim()
+    .isLength({ min: 3 })
+    .escape(),
+
+  // Process request after validation and sanitization.
+  asyncHandler(async (req, res, next) => {
+    // Extract the validation errors from a request.
+    const errors = validationResult(req);
+
+    // Create a category object with escaped and trimmed data.
+    const category = new Category({ name: req.body.name });
+
+    if (!errors.isEmpty()) {
+      // There are errors. Render the form again with sanitized values/error messages.
+      res.render("category_form", {
+        title: "Create Category",
+        category: category,
+        errors: errors.array(),
+      });
+      return;
+    } else {
+      // Data from form is valid.
+      // Check if Category with same name already exists.
+      const categoryExists = await Category.findOne({
+        name: req.body.name,
+      }).exec();
+      if (categoryExists) {
+        // Category exists, redirect to its detail page.
+        res.redirect(categoryExists.url);
+      } else {
+        await category.save();
+        // New category saved. Redirect to category detail page.
+        res.redirect(category.url);
+      }
+    }
+  }),
+];
+
+// Display Category delete form on GET.
+exports.category_delete_get = asyncHandler(async (req, res, next) => {
+  res.send("NOT IMPLEMENTED: Category delete GET");
+});
+
+// Handle Category delete on POST.
+exports.category_delete_post = asyncHandler(async (req, res, next) => {
+  res.send("NOT IMPLEMENTED: Category delete POST");
+});
+
+// Display Category update form on GET.
+exports.category_update_get = asyncHandler(async (req, res, next) => {
+  res.send("NOT IMPLEMENTED: Category update GET");
+});
+
+// Handle Category update on POST.
+exports.category_update_post = asyncHandler(async (req, res, next) => {
+  res.send("NOT IMPLEMENTED: Category update POST");
+});
--- routes/catalog.js
+++ routes/catalog.js
@@ -5,6 +5,7 @@ const router = express.Router();
 const item_controller = require("../controllers/itemController");
 const creator_controller = require("../controllers/creatorController");
 const type_controller = require("../controllers/typeController");
+const category_controller = require("../controllers/categoryController");
 // const item_instance_controller = require("../controllers/itemInstanceController");
 
 /// item ROUTES ///
@@ -87,6 +88,32 @@ router.get("/creators", creator_controller.creator_list);
 
 // GET request for list of all Types.
 router.get("/types", type_controller.type_list);
+
+/// CATEGORY ROUTES ///
+
+// // GET request for creating a Category. NOTE This must come before route that displays Category (uses id).
+// router.get("/category/create", category_controller.category_create_get);
+
+// //POST request for creating Category.
+// router.post("/category/create", category_controller.category_create_post);
+
+// // GET request to delete Category.
+// router.get("/category/:id/delete", category_controller.category_delete_get);
+
+// // POST request to delete Category.
+// router.post("/category/:id/delete", category_controller.category_delete_post);
+
+// // GET request to update Category.
+// router.get("/category/:id/update", category_controller.category_update_get);
+
+// // POST request to update Category.
+// router.post("/category/:id/update", category_controller.category_update_post);
+
+// // GET request for one Category.
+// router.get("/category/:id", category_controller.category_detail);
+
+// GET request for list of all Categories.
+router.get("/categories", category_controller.category_list);
 
 /// BOOKINSTANCE ROUTES ///
 
